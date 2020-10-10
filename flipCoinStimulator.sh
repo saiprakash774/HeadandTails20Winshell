49878e477d1baaa67b096f2147dba6fa4c083a81
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-11 01:54:19 +0530

    heads&tails count on multiple flips

--- flipCoinStimulator.sh
+++ flipCoinStimulator.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-a=$(( RANDOM%2 ));
-if [ $a == 1 ];
-then
-         echo "heads are winner"
-else
-        echo "tails are winner"
-fi
+heads=0;
+tails=0;
+read -p "Enter number of flip required " n
+for (( i=0; i<n; i++ ))
+do
+	a=$(( RANDOM%2 ));
+	if [ $a == 1 ];
+	then
+         	heads=$(($heads+1))
+	else
+        	tails=$(($tails+1))
+	fi
+done
+
+echo "Number of head win" $heads
+echo "Number of tail win" $tails
